44207490fea9b2e30c575e041a439f71d6e0ef56
Jurek Weber <jurek@bessermailer.de>
2015-07-10 04:44:31 -0400

    no swiping sucks

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -3,3 +3,16 @@
 */
 var days = ["","","","",""]; // 5 days the week
 var daysNumber = 5; // 5 Days ;)
+var disableSwiping = false; // true/false
+
+
+
+
+
+
+
+
+
+if (disableSwiping) {
+    $(".body").on("touchmove", false);
+}
